27e7a659c64e95c77492c222a265dfa61f56a4ef
LionyxML <rahul.juliato@gmail.com>
2021-05-11 19:51:48 -0300

    Initial setup of views and routes. Basic scss.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <div class="card">
+      <h4>Dad jokes!</h4>
+      <h2>Here goes the jokes</h2>
+      <a href="#">
+        <div class="btn">
+          Another Joke
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
---
+++ src/views/Jokes.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="Jokes">
+    <p>Rota de Jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
